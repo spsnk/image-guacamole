fa70c880530a60b7d99be2ae79dd44acc8fa135c
spsnk <salvador.paz.santos@gmail.com>
2018-10-14 12:10:48 -0500

    Loading fixed

--- js/main.js
+++ js/main.js
@@ -6,8 +6,7 @@ var isAdvancedUpload = function() {
 $(document).ready(function()
 {
   var image = [];
-  var $form;
-  $form = $('.box');
+  var $form = $('.box');
 
   if (isAdvancedUpload) {
     $form.addClass('has-advanced-upload');
@@ -16,8 +15,7 @@ $(document).ready(function()
   if (isAdvancedUpload) {
 
     var droppedFiles = false;
-    var canvas  = document.getElementById("canvas_1");
-    var context = canvas.getContext("2d");
+    var loaded_image;
 
     $form.on('drag dragstart dragend dragover dragenter dragleave drop', function(e) {
       e.preventDefault();
@@ -34,17 +32,33 @@ $(document).ready(function()
       console.log("Dropped: " + droppedFile.name + " MIME: " + droppedFile.type);
       var fr = new FileReader();
       fr.onload = function(ev2) {
-          var img = new Image();
-          img.addEventListener("load", function() {
-            context.drawImage(img, 0, 0);
+          loaded_image = new Image();
+          loaded_image.addEventListener("load", function() {
+            //context.drawImage(loaded_image, 0, 0);
+            console.log("Image "+droppedFile.name+" loaded");
           });
-          img.src = ev2.target.result;
-          //$('#img1').attr('src', ev2.target.result);
+          loaded_image.src = ev2.target.result;
+          $('#preview').attr('src', loaded_image.src).css("display", "block");;
+          $(".box__icon").hide();
       };
       if(droppedFile.type.includes("image") )
       {
         fr.readAsDataURL(droppedFile);
       }
+    });
+    $("#slot1").on('click',function(e){
+      var canvas  = document.getElementById("canvas_1");
+      var context = canvas.getContext("2d");
+      canvas.width = loaded_image.width;
+      canvas.height = loaded_image.height;
+      context.drawImage(loaded_image, 0, 0);
+    });
+    $("#slot2").on('click',function(e){
+      var canvas  = document.getElementById("canvas_2");
+      var context = canvas.getContext("2d");
+      canvas.width = loaded_image.width;
+      canvas.height = loaded_image.height;
+      context.drawImage(loaded_image, 0, 0);
     });
   }
 });
