5cb2f4b3c3050f7c9ce6d91797e37f890e4403b2
Salvador P <nyakebei.ipn@gmail.com>
2018-12-03 13:04:39 -0600

    Treating channels independently

--- js/image_functions.js
+++ js/image_functions.js
@@ -69,28 +69,25 @@ function pixelAdd ( image1, image2 )
 {
   var newHeight = image1.height<image2.height?image1.height:image2.height;
   var newWidth  = image1.width<image2.width?image1.width:image2.width;
-  
-  var buf1 = image1.data.buffer;
-  var data1 = new Uint32Array(buf1);
-  var buf2 = image2.data.buffer;
-  var data2 = new Uint32Array(buf2);
-  
-  var newbuf  = new ArrayBuffer( newHeight * newWidth * 4 );
-  var newbuf8 = new Uint8ClampedArray(newbuf);
-  var newdata = new Uint32Array( newbuf );
-  
+  var data1 = image1.data;
+  var data2 = image2.data;
+  var newdata = new Uint8ClampedArray( newHeight * newWidth * 4 );
   for (var index1 = 0, index2 = 0,i=0,j=0; i < newdata.length;)
   {
-    newdata[i++] = data1[index1++]+data2[index2++];
+    newdata[i++] = data1[index1++] + data2[index2++];
+    newdata[i++] = data1[index1++] + data2[index2++];
+    newdata[i++] = data1[index1++] + data2[index2++];
+    newdata[i] = 255;
+    i++;
+    index1++;
+    index2++;
     if(i%newWidth==0)
     {
       index1 = ++j * image1.width;
       index2 = j * image2.width;
     }
   }
-  
-  var newImage = new ImageData(newbuf8, newWidth, newHeight);
-  
+  var newImage = new ImageData(newdata, newWidth, newHeight);
   return newImage;
 }
 
@@ -98,28 +95,25 @@ function pixelSubstract ( image1, image2 )
 {
   var newHeight = image1.height<image2.height?image1.height:image2.height;
   var newWidth  = image1.width<image2.width?image1.width:image2.width;
-  
-  var buf1 = image1.data.buffer;
-  var data1 = new Uint32Array(buf1);
-  var buf2 = image2.data.buffer;
-  var data2 = new Uint32Array(buf2);
-  
-  var newbuf  = new ArrayBuffer( newHeight * newWidth * 4 );
-  var newbuf8 = new Uint8ClampedArray(newbuf);
-  var newdata = new Uint32Array( newbuf );
-  
+  var data1 = image1.data;
+  var data2 = image2.data;
+  var newdata = new Uint8ClampedArray( newHeight * newWidth * 4 );
   for (var index1 = 0, index2 = 0,i=0,j=0; i < newdata.length;)
   {
-    newdata[i++] = data1[index1++]-data2[index2++];
+    newdata[i++] = data1[index1++] - data2[index2++];
+    newdata[i++] = data1[index1++] - data2[index2++];
+    newdata[i++] = data1[index1++] - data2[index2++];
+    newdata[i] = 255;
+    i++;
+    index1++;
+    index2++;
     if(i%newWidth==0)
     {
       index1 = ++j * image1.width;
       index2 = j * image2.width;
     }
   }
-  
-  var newImage = new ImageData(newbuf8, newWidth, newHeight);
-  
+  var newImage = new ImageData(newdata, newWidth, newHeight);
   return newImage;
 }
 
@@ -128,28 +122,25 @@ function pixelMultiply ( image1, image2 )
 {
   var newHeight = image1.height<image2.height?image1.height:image2.height;
   var newWidth  = image1.width<image2.width?image1.width:image2.width;
-  
-  var buf1 = image1.data.buffer;
-  var data1 = new Uint32Array(buf1);
-  var buf2 = image2.data.buffer;
-  var data2 = new Uint32Array(buf2);
-  
-  var newbuf  = new ArrayBuffer( newHeight * newWidth * 4 );
-  var newbuf8 = new Uint8ClampedArray(newbuf);
-  var newdata = new Uint32Array( newbuf );
-  
+  var data1 = image1.data;
+  var data2 = image2.data;
+  var newdata = new Uint8ClampedArray( newHeight * newWidth * 4 );
   for (var index1 = 0, index2 = 0,i=0,j=0; i < newdata.length;)
   {
     newdata[i++] = data1[index1++] * data2[index2++];
+    newdata[i++] = data1[index1++] * data2[index2++];
+    newdata[i++] = data1[index1++] * data2[index2++];
+    newdata[i] = 255;
+    i++;
+    index1++;
+    index2++;
     if(i%newWidth==0)
     {
       index1 = ++j * image1.width;
       index2 = j * image2.width;
     }
   }
-  
-  var newImage = new ImageData(newbuf8, newWidth, newHeight);
-  
+  var newImage = new ImageData(newdata, newWidth, newHeight);
   return newImage;
 }
 
@@ -168,19 +159,19 @@ function pixelAnd ( image1, image2 )
 {
   var newHeight = image1.height<image2.height?image1.height:image2.height;
   var newWidth  = image1.width<image2.width?image1.width:image2.width;
-  
-  var buf1 = image1.data.buffer;
-  var data1 = new Uint32Array(buf1);
-  var buf2 = image2.data.buffer;
-  var data2 = new Uint32Array(buf2);
-  
-  var newbuf  = new ArrayBuffer( newHeight * newWidth * 4 );
-  var newbuf8 = new Uint8ClampedArray(newbuf);
-  var newdata = new Uint32Array( newbuf );
+  var data1 = image1.data;
+  var data2 = image2.data;
+  var newdata = new Uint8ClampedArray( newHeight * newWidth * 4 );
   
   for (var index1 = 0, index2 = 0,i=0,j=0; i < newdata.length;)
   {
     newdata[i++] = data1[index1++] & data2[index2++];
+    newdata[i++] = data1[index1++] & data2[index2++];
+    newdata[i++] = data1[index1++] & data2[index2++];
+    newdata[i] = 255;
+    i++;
+    index1++;
+    index2++;
     if(i%newWidth==0)
     {
       index1 = ++j * image1.width;
@@ -188,7 +179,7 @@ function pixelAnd ( image1, image2 )
     }
   }
   
-  var newImage = new ImageData(newbuf8, newWidth, newHeight);
+  var newImage = new ImageData(newdata, newWidth, newHeight);
   
   return newImage;
 }
@@ -198,28 +189,25 @@ function pixelOr ( image1, image2 )
 {
   var newHeight = image1.height<image2.height?image1.height:image2.height;
   var newWidth  = image1.width<image2.width?image1.width:image2.width;
-  
-  var buf1 = image1.data.buffer;
-  var data1 = new Uint32Array(buf1);
-  var buf2 = image2.data.buffer;
-  var data2 = new Uint32Array(buf2);
-  
-  var newbuf  = new ArrayBuffer( newHeight * newWidth * 4 );
-  var newbuf8 = new Uint8ClampedArray(newbuf);
-  var newdata = new Uint32Array( newbuf );
-  
+  var data1 = image1.data;
+  var data2 = image2.data;
+  var newdata = new Uint8ClampedArray( newHeight * newWidth * 4 );
   for (var index1 = 0, index2 = 0,i=0,j=0; i < newdata.length;)
   {
     newdata[i++] = data1[index1++] | data2[index2++];
+    newdata[i++] = data1[index1++] | data2[index2++];
+    newdata[i++] = data1[index1++] | data2[index2++];
+    newdata[i] = 255;
+    i++;
+    index1++;
+    index2++;
     if(i%newWidth==0)
     {
       index1 = ++j * image1.width;
       index2 = j * image2.width;
     }
   }
-  
-  var newImage = new ImageData(newbuf8, newWidth, newHeight);
-  
+  var newImage = new ImageData(newdata, newWidth, newHeight);
   return newImage;
 }
 
@@ -231,23 +219,4 @@ function pixelXor ( image1, image2 )
   newImage.data = pixelNot ( newImage.data );
   
   return newImage;
-}
-
-function get2d ( imageData )
-{
-  var buf = new ArrayBuffer(imageData.data.length);
-  var buf8 = new Uint8ClampedArray(buf);
-  var data = new Uint32Array(buf);
-  
-  var newdata = new Uint32Array( imageData.width );
-  
-  for(var i=0,index=0;i<imageData.width;i++)
-  {
-    newdata[i] = new Uint32Array( imageData.height );
-    for(var j=0;j<imageData.height;j++)
-    {
-      newdata[i][j] = data[index++];
-    }
-  }
-  return newdata;
 }
